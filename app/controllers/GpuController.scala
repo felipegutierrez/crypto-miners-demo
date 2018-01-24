0af313f8ff0947db4757b7ac41a70d8ec76a7f24
Felipe Oliveira Gutierrez <felipe.o.gutierrez@gmail.com>
2018-01-24 11:01:59 -0300

    Using flatMap and map to get add Gpu to a Rack and catching Exceptions.

--- app/controllers/GpuController.scala
+++ app/controllers/GpuController.scala
@@ -22,41 +22,37 @@ class GpuController @Inject()(cc: ControllerComponents, rackRepository: RackRepo
     either.fold(
       errors => Future.successful(BadRequest("invalid json Gpu.\n")),
       gpu => {
-        try {
-          val futureResult = for {
-            futureRackRow <- rackRepository.getById(gpu.rackId) recoverWith {
-              case e: Exception => throw RackException(s"Error on select Rack: ${e.getMessage}")
+        rackRepository.getById(gpu.rackId).flatMap {
+          case Some(rackRow) =>
+            // If the Rack exists we add a Gpu
+            // given time (should be after currentHour, which was set during setup)
+            if (Util.toTime(gpu.installedAt) < rackRow.currentHour) {
+              throw new GpuException("Given time [" + gpu.installedAt + "] is not after the Rack currentHour [" + Util.toDate(rackRow.currentHour) + "].")
             }
-            futureSeqGpuRow <- gpuRepository.getByRack(futureRackRow.get.id) recoverWith {
-              case e: Exception => throw GpuException(s"Error on select Gpu's from Rack: ${e.getMessage}")
-            }
-          } yield (futureRackRow, futureSeqGpuRow)
-          futureResult.map {
-            case (Some(rackRow), seqGpuRow) =>
-              // given time (should be after currentHour, which was set during setup)
-              if (Util.toTime(gpu.installedAt) < rackRow.currentHour) {
-                throw GpuException("Given time is not after the currentHour.")
-              }
-              // Calculate the Gpu produced based on the profitPerGpu
-              val produced = if (gpu.produced == 0.toFloat) {
-                val times = ((Util.toTime(gpu.installedAt) - rackRow.currentHour) / 3600000) + 1
-                rackRepository.getProfitPerGpu * times
-              } else gpu.produced
+            // Calculate the Gpu produced based on the profitPerGpu
+            val produced = if (gpu.produced == 0.toFloat) {
+              val times = ((Util.toTime(gpu.installedAt) - rackRow.currentHour) / 3600000) + 1
+              rackRepository.getProfitPerGpu * times
+            } else gpu.produced
+
+            // Get all the Gpu of the Rack to create the GpuId and update the Rack.produced
+            gpuRepository.getByRack(rackRow.id).map { seqGpuRow: Seq[GpuRow] =>
               // Insert Gpu in the Rack
-              val gpuRow = GpuRow(rackRow.id + "-gpu-" + seqGpuRow.size, rackRow.id, produced, Util.toTime(gpu.installedAt))
-              gpuRepository.insert(gpuRow)
+              gpuRepository.insert(GpuRow(rackRow.id + "-gpu-" + seqGpuRow.size, rackRow.id, produced, Util.toTime(gpu.installedAt)))
               // update produced from Rack as a sum of all gpu produced
               val total = seqGpuRow.map(_.produced).sum + produced
               rackRepository.update(rackRow.id, Some(total), None)
-              Ok
-            case (None, seqGpuRow) => BadRequest("Rack not found")
-          }
-        } catch {
-          case pe: ParseException => Future.successful(BadRequest(s"Could not parse the date: ${pe.getMessage}\n"))
-          case re: RackException => Future.successful(BadRequest(s"Rack Exception: ${re.getMessage}\n"))
-          case ge: GpuException => Future.successful(BadRequest(s"Gpu Exception: ${ge.getMessage}\n"))
-          case nsee: NoSuchElementException => Future.successful(BadRequest(s"Element not found.\n"))
-          case e: Exception => Future.successful(BadRequest(s"Exception found: ${e.getMessage}\n"))
+            }
+            Future.successful(Ok("Gpu added to the Rack [" + gpu.rackId + "].\n"))
+          case None =>
+            // If the Rack does not exist throw an Exception
+            throw new RackException("Rack [" + gpu.rackId + "] does not exist.")
+        }.recover {
+          case ge: GpuException => BadRequest(s"Gpu Exception: ${ge.getMessage}\n")
+          case re: RackException => BadRequest(s"Rack Exception: ${re.getMessage}\n")
+          case pe: ParseException => BadRequest(s"Could not parse the date: ${pe.getMessage}\n")
+          case e: Exception => BadRequest(Json.toJson("Error to add Gpu: " + e.getMessage))
+          case _ => BadRequest(Json.toJson("Unknown error to add Gpu."))
         }
       }
     )
