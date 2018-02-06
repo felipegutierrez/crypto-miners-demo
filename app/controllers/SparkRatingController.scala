cd0a0e40c1a2daf2da1c7d6f1ae7699276984e21
Felipe Oliveira Gutierrez <felipe.o.gutierrez@gmail.com>
2018-02-06 17:25:49 -0300

    Create a broadcast variable of our ID -> movie name map to our entire cluster.

--- app/controllers/SparkRatingController.scala
+++ app/controllers/SparkRatingController.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import java.nio.charset.CodingErrorAction
 import javax.inject._
 
 import bootstrap.SparkCommons
@@ -7,6 +8,8 @@ import models.helper.Util
 import org.apache.spark.sql.DataFrame
 import play.api.i18n.I18nSupport
 import play.api.mvc._
+
+import scala.io.{Codec, Source}
 
 class SparkRatingController @Inject()(cc: ControllerComponents) extends AbstractController(cc) with I18nSupport {
 
@@ -68,11 +71,31 @@ class SparkRatingController @Inject()(cc: ControllerComponents) extends Abstract
     Ok(toJsonString(results))
   }
 
+  // Load up a Map of movies IDs to movie names
+  def loadMovieNames(): Map[Int, String] = {
+    // handle character encoding issue
+    implicit val codec = Codec("UTF-8")
+    codec.onMalformedInput(CodingErrorAction.REPLACE)
+    codec.onUnmappableCharacter(CodingErrorAction.REPLACE)
+
+    // create a map of Ints to Strings and populate it from u.item
+    var movieNames: Map[Int, String] = Map()
+    val lines = Source.fromFile("downloads/ml-100k/u.item").getLines()
+    for (line <- lines) {
+      val fields = line.split('|')
+      if (fields.length > 1) movieNames += (fields(0).toInt -> fields(1))
+    }
+    movieNames
+  }
+
   def popularMovies() = Action { implicit request: Request[AnyContent] =>
     Util.downloadSourceFile("downloads/ml-100k.zip", "http://files.grouplens.org/datasets/movielens/ml-100k.zip")
     Util.unzip("downloads/ml-100k.zip")
 
     val sparkContext = SparkCommons.sparkSession.sparkContext // got sparkContext
+
+    // Create a broadcast variable of our ID -> movie name map to our entire cluster
+    val nameDict = sparkContext.broadcast(loadMovieNames)
 
     val popularMovies = sparkContext
       .textFile("downloads/ml-100k/u.data") // popularMovies
@@ -80,6 +103,7 @@ class SparkRatingController @Inject()(cc: ControllerComponents) extends Abstract
       .reduceByKey((x, y) => x + y) // Count up all the 1's for each movie
       .map(x => (x._2, x._1)) // Flip (movieId, count) to (count, movieId)
       .sortByKey(false) // Sort and invert the order to show the most popular movies first
+      .map(x => (nameDict.value(x._2), x._1)) // Fold in the movie names from the broadcast variable
 
     // collect and print the result
     val results = popularMovies.collect().toList.mkString(",\n")
